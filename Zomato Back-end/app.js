fa633978e64b6ee353b9b7e7fecd0af2393fe098
Siddhantmishraa <siddhaantmishraa@gmail.com>
2024-04-16 00:37:55 +0530

    Source

---
+++ Zomato Back-end/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const AppRoutes = require("./routes/AppRoutes");
+
+const app = express();
+const PORT = 3030;
+const MONGODB_URI = `mongodb://localhost:27017`;
+
+app.use(cors());
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api", AppRoutes);
+
+mongoose.connect(MONGODB_URI).then(() => {
+    console.log("database connected successfully.");
+    app.listen(PORT, () => {
+      console.log("Project is running on port ", PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
