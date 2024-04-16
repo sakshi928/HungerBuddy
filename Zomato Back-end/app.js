0b07ec1c718b12ff2b78ef86bd60ce70bedb9fd9
Siddhantmishraa <siddhaantmishraa@gmail.com>
2024-04-16 12:31:32 +0530

    new

--- Zomato Back-end/app.js
+++ Zomato Back-end/app.js
@@ -5,7 +5,7 @@ const AppRoutes = require("./routes/AppRoutes");
 
 const app = express();
 const PORT = 3030;
-const MONGODB_URI = `mongodb://localhost:27017`;
+const MONGODB_URI = `mongodb://127.0.0.1:27017`;
 
 app.use(cors());
 app.use(express.json()); 
